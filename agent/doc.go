188db0b5532e4b7ad186ff9e5b3a781f1a23a581
Ric Szopa <Ric Szopa ryszard.szopa@gmail.com>
2023-06-01 12:19:27 +0200

    Added package documentation.

---
+++ agent/doc.go
@@ -0,0 +1,13 @@
+// Package agent provides a comprehensive and structured interface to the OpenAI
+// API in Go, enabling easy creation and management of Language Learning Model
+// (LLM) based agents. It's designed to aid the development of complex systems,
+// like autonomous agents that can interact and perform a range of AI tasks.
+//
+// This package contains several utility functions, including but not limited
+// to, caching, templating, and configurable retries. These functionalities
+// provide an increased level of control and flexibility in defining agent
+// behaviors.
+//
+// Please note that this package is currently experimental and is not yet
+// suitable for production use. The API is subject to change without notice.
+package agent
