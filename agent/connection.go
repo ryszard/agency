65533df150101b904fb715472bae7a26f95fc5a2
Ric Szopa <Ric Szopa ryszard.szopa@gmail.com>
2023-06-01 14:19:18 +0200

    RateLimitingClient.

--- agent/connection.go
+++ agent/connection.go
@@ -80,3 +80,40 @@ func (client *retryingClient) CreateChatCompletionStream(ctx context.Context, re
 	}
 	return resp.(*openai.ChatCompletionStream), nil
 }
+
+// Limiter is an interface for rate limiters. A recommended implementation is
+// golang.org/x/time/rate.Limiter .
+type Limiter interface {
+	Wait(ctx context.Context) error
+}
+
+type rateLimitingClient struct {
+	client  Client
+	limiter Limiter
+}
+
+// RateLimiting wraps a Client and rate limits requests using the provided
+// limiter. If the limiter returns an error, RateLimiting will return that
+// error, otherwise it will return the error from the wrapped Client.
+func RateLimiting(client Client, limiter Limiter) Client {
+	return &rateLimitingClient{
+		client:  client,
+		limiter: limiter,
+	}
+}
+
+func (client *rateLimitingClient) CreateChatCompletion(ctx context.Context, req openai.ChatCompletionRequest) (openai.ChatCompletionResponse, error) {
+	log.WithField("req", req).Info("CreateChatCompletion")
+	if err := client.limiter.Wait(ctx); err != nil {
+		return openai.ChatCompletionResponse{}, err
+	}
+	return client.client.CreateChatCompletion(ctx, req)
+}
+
+func (client *rateLimitingClient) CreateChatCompletionStream(ctx context.Context, req openai.ChatCompletionRequest) (*openai.ChatCompletionStream, error) {
+	log.WithField("req", req).Info("CreateChatCompletionStream")
+	if err := client.limiter.Wait(ctx); err != nil {
+		return nil, err
+	}
+	return client.client.CreateChatCompletionStream(ctx, req)
+}
