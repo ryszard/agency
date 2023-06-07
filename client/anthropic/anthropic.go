e39346b9d27a2e70e8c0db00b77d1b4c670706c1
Ric Szopa <Ric Szopa ryszard.szopa@gmail.com>
2023-06-07 16:33:39 +0200

    Model constants for Anthropic.

--- client/anthropic/anthropic.go
+++ client/anthropic/anthropic.go
@@ -10,6 +10,46 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	// Our largest model, ideal for a wide range of more complex tasks.
+	ClaudeV1 = "claude-v1"
+
+	// An enhanced version of ClaudeV1 with a 100,000 token context window.
+	ClaudeV1_100k = "claude-v1-100k"
+
+	// A smaller model with far lower latency, sampling at roughly 40 words/sec!
+	ClaudeInstantV1 = "claude-instant-v1"
+
+	// An enhanced version of ClaudeInstantV1 with a 100,000 token context window.
+	ClaudeInstantV1_100k = "claude-instant-v1-100k"
+
+	// More robust against red-team inputs, better at precise instruction-following,
+	// better at code, and better and non-English dialogue and writing.
+	ClaudeV1_3 = "claude-v1.3"
+
+	// An enhanced version of ClaudeV1_3 with a 100,000 token context window.
+	ClaudeV1_3_100k = "claude-v1.3-100k"
+
+	// An improved version of ClaudeV1, slightly improved at general helpfulness,
+	// instruction following, coding, and other tasks. It is also considerably
+	// better with non-English languages.
+	ClaudeV1_2 = "claude-v1.2"
+
+	// An earlier version of ClaudeV1.
+	ClaudeV1_0 = "claude-v1.0"
+
+	// Latest version of ClaudeInstantV1. It is better at a wide variety of tasks
+	// including writing, coding, and instruction following. It performs better on
+	// academic benchmarks, including math, reading comprehension, and coding tests.
+	ClaudeInstantV1_1 = "claude-instant-v1.1"
+
+	// An enhanced version of ClaudeInstantV1_1 with a 100,000 token context window.
+	ClaudeInstantV1_1_100k = "claude-instant-v1.1-100k"
+
+	// An earlier version of ClaudeInstantV1.
+	ClaudeInstantV1_0 = "claude-instant-v1.0"
+)
+
 type Client struct {
 	client *anthropic.Client
 }
